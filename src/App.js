45bf5d0e4bb5dfc54e260d92004749f3efc1d222
MONOJIT PALIT <monojitpalit4@gmail.com>
2025-06-30 00:38:07 +0530

    removed text

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Login } from "./Components/Login"
 import { Home } from './Components/Home';
 import { ToastContainer } from 'react-toastify';
-// this is app.j
 function App() {
   return (
     <div>
